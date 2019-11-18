b8e8e31467a33270da635718c5c73e4077736871
Carlos Arvelo García <carlosarvelogarcia.ag@gmail.com>
2019-11-18 20:51:45 +0000

    travis deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 COMMAND="cd ${REMOTE_PATH} && git pull"
 
-ssh -o StrictHostKeyChecking=no -i deploy-travis -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
+ssh -o StrictHostKeyChecking=no -i sshTravis -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
